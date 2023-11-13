32a7cb29710c4b59bb4c652ef80af82a20c71f61
Nevereverh8 <nevereverh8@gmail.com>
2023-11-13 20:21:44 +0300

    small changes to db_requests.py

---
+++ db_requests.py
@@ -0,0 +1,135 @@
+import sqlite3 as sl
+
+# There's will be json file path
+db = sl.connect('deitabeiza.db')
+
+if __name__ == '__main__':
+    category_list = ['Напитки', 'Курица', 'Мясо', 'Рыба', 'Салаты', 'Алкогольные напитки', 'Пиццы', 'Соусы', 'Десерты']
+    food_list = [['Кока-кола 0.5л в стекле', 2.50, 0, 1, 5], ['Фанта 0.5л в стекле', 2.50, 0, 1, 5],
+                 ['Куриные наггетсы 9 шт', 7.99, 0, 2, 20], ['Куриные наггетсы 15 шт', 10.49, 0, 2, 20],
+                 ['Свинныя отбивные 350 гр', 7.99, 0, 3, 25], ['Мясо по французски 350 гр', 9.99, 0, 3, 25],
+                 ['Филе хека 300 гр', 9.34, 0, 4, 40], ['Запеченый лосось 500 г', 20.99, 0, 4, 50],
+                 ['Мимоза 350 гр', 6.20, 0, 5, 20], ['Селедь под шубой 350 гр', 7.09, 0, 5, 20],
+                 ['Пиво Stella Artois 350 гр', 5.20, 0, 6, 5], ['Вино Alazan Valley 350 гр', 5.20, 0, 6, 20],
+                 ['Пицца 4 сыра 700 гр', 15.30, 0, 7, 30], ['Пицца маргарита 700 гр', 14.10, 0, 7, 30]]
+
+    with db as con:
+        # Orders
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Orders(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        client_id INTEGER,
+                        time_placed TEXT,
+                        delivery_time TEXT,
+                        is_finished INTEGER,
+                        is_aborted INTEGER,
+                        admin_processed,
+                        total_price REAL)
+                        ''')
+
+        # Order_lists
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Order_lists(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        food_id INTEGER,
+                        amount float,
+                        order_id INTEGER)
+                        ''')
+
+        # Food
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Food(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        name TEXT,
+                        price REAL,
+                        stop_flag INTEGER,
+                        category_id INTEGER,
+                        cook_time INTEGER)
+                        ''')
+        sql_insert = '''INSERT INTO Food (name, price, stop_flag, category_id, cook_time) VALUES (?,?,?,?,?)'''
+        for i in food_list:
+            con.execute(sql_insert, i)
+
+        # Categories
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Categories(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        name TEXT)
+                        ''')
+        sql_insert = '''INSERT INTO Categories (name) VALUES (?)'''
+        for i in category_list:
+            con.execute(sql_insert, [i])
+
+        # Admins
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Admins(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        name TEXT,
+                        role INTEGER,
+                        tg_id INTEGER)
+                        ''')
+
+        # Order_reviews
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Order_reviews(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        order_id INTEGER,
+                        client_id INTEGER,
+                        rating INTEGER,
+                        comment TEXT)
+                        ''')
+
+        # Clients
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Clients(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        name TEXT,
+                        role INTEGER,
+                        tg_id INTEGER)
+                        ''')
+
+        # Food_reviews
+        con.execute('''
+                        CREATE TABLE IF NOT EXISTS Food_reviews(
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        food_id INTEGER,
+                        client_id INTEGER,
+                        rating INTEGER,
+                        comment TEXT)
+                        ''')
+
+
+# further below there will be the most common requests
+# to make it easier to implement this to bots
+
+
+def get_categories():
+    """
+    Returns list of all categories
+    :return:
+    """
+    with db as con:
+        categories = con.execute('''SELECT name FROM Categories''')
+        categories = categories.fetchall()
+        return [i[0] for i in categories]
+
+
+def get_category(category: str):
+    """
+    Returns dict of given category {name: price, ...}
+    """
+    with db as con:
+        food = con.execute(f'''
+                                     SELECT name, price FROM Food
+                                     WHERE category_id = (SELECT id FROM Categories
+                                                          WHERE name = '{category}')
+                                     ''')
+        food = food.fetchall()
+        dict_of_food = {}
+        for i in food:
+            dict_of_food[i[0]] = i[1]
+        return food
+
+
+def insert_order(client_id: int, time_placed: int, admin_id: int, order_list: dict):
+    pass
