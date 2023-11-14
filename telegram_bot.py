1bce4f7f942728eeb8ed067bdc5114866afa3ae6
Sterdam <novkos2306@gmail.com>
2023-11-14 16:38:11 +0300

    deleted:    kost.py 	new file:   telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from db_requests import *
+
+bot = telebot.TeleBot('6566836113:AAEROPk40h1gT7INUnWNPg2LEbYug6uDbns');
+
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/start':
+        a = get_categories()
+        print(a)
+        bot.send_message(message.chat.id,"Тут будет инфа о ресторане!")
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    bot.answer_callback_query(callback_query_id=call.id)
+    
+print("Ready")
+bot.infinity_polling()    
+
