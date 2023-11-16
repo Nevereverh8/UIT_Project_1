fc1c97337c20ed5fc2ca00328f176aee3b3bc533
Nevereverh8 <nevereverh8@gmail.com>
2023-11-16 23:54:10 +0300

    Added main.py, where added threading

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 from db_requests import *
-#print('hello gays')
-#print(get_categories())
-#print(get_category('Напитки'))
+import threading
+from scheduler import Scheduler
+
+
+def vk():
+    # import vk
+    pass
+
+
+def tg():
+    # import telegram_bot
+    pass
+
+
+# tg_thread = threading.Thread(target=tg)
+# vk_thread = threading.Thread(target=vk)
+# tg_thread.start()
+# vk_thread.start()
+
+
+# print('hello gays')
+# print(get_categories())
+# print(get_category('Напитки'))
 # insert_order(1, '14.11.2023 6:51', 1, {'Кока-кола 0.5л в стекле': 2,
 #                                         "Фанта 0.5л в стекле": 3})
-with db as con:
-    pass
+
+# with db as con:
+#     pass
     # print(con.execute('SELECT * FROM Orders').fetchall()[0])
     # print(con.execute('SELECT * FROM Order_lists').fetchall()[0])
+
+
 print(get_client(123456789))
 print(get_client(123456788))
+
+
