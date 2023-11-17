32981c8a7f3d4136e2f1fe19e8b8bbd872c96a40
Nevereverh8 <nevereverh8@gmail.com>
2023-11-18 01:30:56 +0300

    Added stop_food and unstop_food functions Reworked insert_order function: Now returns delivery_time Better function dockstring description Added some comments

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def tg():
 # print('hello gays')
 # print(get_categories())
 # print(get_category('Напитки'))
-# insert_order(1, '14.11.2023 6:51', 1, {'Кока-кола 0.5л в стекле': 2,
-#                                         "Фанта 0.5л в стекле": 3})
+a = insert_order(1, '14.11.2023 6:51', 1, {"Кока-кола 0.5л в стекле": 2,
+                                           "Фанта 0.5л в стекле": 3})
+print(a)
+with db as con:
+    # pass
+    print(con.execute('SELECT * FROM Orders').fetchall())
+    print(con.execute('SELECT * FROM Order_lists').fetchall())
 
-# with db as con:
-#     pass
-    # print(con.execute('SELECT * FROM Orders').fetchall()[0])
-    # print(con.execute('SELECT * FROM Order_lists').fetchall()[0])
 
-
-print(get_client(123456789))
-print(get_client(123456788))
+# print(get_client(123456789))
+# print(get_client(123456788))
 
 
