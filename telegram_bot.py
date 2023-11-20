0e72065fa6d834124bbfc60671a18057b8fe831e
Sterdam <novkos2306@gmail.com>
2023-11-20 22:42:39 +0300

    Small changes and fixes

--- telegram_bot.py
+++ telegram_bot.py
@@ -39,7 +39,7 @@ def gen_foods(food, chat_id, temp=0):
         temp = sessions[chat_id]['cart'][dish]
     keyb_food = InlineKeyboardMarkup()
     keyb_food.add(InlineKeyboardButton('-', callback_data=f'fa;{food[0]};-'), InlineKeyboardButton(f'Кол-во: {temp}', callback_data='None'),InlineKeyboardButton('+', callback_data=f'fa;{food[0]};+'))
-    keyb_food.add(InlineKeyboardButton('Добавить в корзину', callback_data=f'f;{food[0]};')) #Добавить в calldatу кол-во товара, из кнопки кол-во
+    keyb_food.add(InlineKeyboardButton('Добавить в корзину', callback_data=f'f;{food[0]};{temp}')) #Добавить в calldatу кол-во товара, из кнопки кол-во
     return keyb_food
    
 # Отдельный слайдер для блюд в категории
@@ -48,7 +48,7 @@ def gen_slider(page, fix_pos=1):
     start = fix_pos*(page-1)
     end = start + fix_pos
     keyb_slider = InlineKeyboardMarkup()
-    keyb_slider.add(InlineKeyboardButton('Назад', callback_data=f'fn-back-{page}'), InlineKeyboardButton('Вперед', callback_data=f'fn-forward-{page}'))
+    keyb_slider.add(InlineKeyboardButton('Назад', callback_data=f'fn-back-{page}'), InlineKeyboardButton('Корзина', callback_data='cart'), InlineKeyboardButton('Вперед', callback_data=f'fn-forward-{page}'))
     keyb_slider.add(InlineKeyboardButton('Вернуться в меню', callback_data='m'))
     return keyb_slider, start, end, page
 
@@ -59,6 +59,7 @@ def start(message):
         sessions[message.chat.id]['last_foods'] = {}
         sessions[message.chat.id]['cart'] = {}
         sessions[message.chat.id]['food_list'] = []
+        sessions[message.chat.id]['real_cart'] = {}
         bot.send_message(message.chat.id, """Приветсвуем в ресторане UIT.\nУютная, доброжелательная атмосфера и достойный сервис  - это основные преимущества ресторана. Все вышеперечисленное и плюс доступный уровень цен позволили заведению оказаться в списке лучших ресторанов Минска xd. \n\n Можете ознакомится с меню, нажав кнопку меню.""", reply_markup=keyb_menu)
 
 # call back types: can be changed
@@ -127,8 +128,7 @@ def query_handler(call):
                 bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, text=call.message.text, reply_markup=gen_foods(call.message.text, call.message.chat.id, sessions[call.message.chat.id]['cart'][call.message.text]))
                 if sessions[call.message.chat.id]['cart'][call.message.text] == 0:
                     sessions[call.message.chat.id]['cart'].pop(call.message.text)
-        print(sessions[call.message.chat.id])
-        sys.stdout.flush()    
+           
 
     # Переключение страниц блюд в категории с помощью слайдера
     # Switching dishes pages in categories with help of slider
@@ -137,16 +137,21 @@ def query_handler(call):
             slider = gen_slider(int(call.data.split('-')[2])+1)
             for f in sessions[call.message.chat.id]['food_list'][slider[1]:slider[2]]:
                 for id in sessions[call.message.chat.id]['last_foods']: 
-                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=id, text=f'{f[0]} цена за шт. - {f[1]}', reply_markup=gen_foods(f, call.message.chat.id, f[0])) 
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=id, text=f'{f[0]} цена за шт. - {f[1]}', reply_markup=gen_foods(f, call.message.chat.id)) 
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Навигация', reply_markup=gen_slider(slider[3])[0])
                     break
         if call.data.split('-')[1] == 'back':
             slider = gen_slider(int(call.data.split('-')[2])-1)
             for f in sessions[call.message.chat.id]['food_list'][slider[1]:slider[2]]:
                 for id in sessions[call.message.chat.id]['last_foods']:
-                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=id, text=f'{f[0]} цена за шт. - {f[1]}', reply_markup=gen_foods(f, call.message.chat.id, f[0])) 
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=id, text=f'{f[0]} цена за шт. - {f[1]}', reply_markup=gen_foods(f, call.message.chat.id)) 
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Навигация', reply_markup=gen_slider(slider[3])[0])
                     break
+    if call.data.split(';')[0] == 'f':
+        sessions[call.message.chat.id]['real_cart'][call.message.text] = call.data.split(';')[2]
+        print(sessions[call.message.chat.id]['real_cart'])
+        sys.stdout.flush() 
+
            
 # Надо добавить корзину.
 # Need to add cart
